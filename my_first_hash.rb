82c42e1732985a2caf08ee420c618799285e56a9
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-28 15:55:47 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+  return shipping_manifest["oil paintings"]
   #your code here!
 end
 
